efea3042f0b5e318e493c79ed6b76f2519b37adf
michael.gearhardt@gmail.com <mcgear>
2021-11-12 11:45:59 -0700

    Checkpoint

--- sidebars.js
+++ sidebars.js
@@ -5,7 +5,7 @@ module.exports = {
       'getting-started/setup',
       'getting-started/create-first-project',
       'getting-started/deploying-project-code',
-      'getting-started/global-edge-network',
+      // 'getting-started/global-edge-network',
     ],
     Guides: [
       'guides/overview',
